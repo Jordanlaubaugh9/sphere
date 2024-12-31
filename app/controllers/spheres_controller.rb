cd22edf65a862fabdfb0fffc12932c03f6a9a2f8
Jordanlaubaugh9 <88402166+Jordanlaubaugh9@users.noreply.github.com>
2024-12-31 12:50:51 +0700

    Add basic auth with Devise allowing user creation with only a name.

--- app/controllers/spheres_controller.rb
+++ app/controllers/spheres_controller.rb
@@ -8,10 +8,28 @@ class SpheresController < ApplicationController
     service = CreateSphereService.new(params[:sphere][:created_by], params[:sphere][:name])
     @sphere = service.call
 
-    if @sphere
-      redirect_to sphere_path(@sphere), notice: 'Sphere was successfully created.'
-    else
-      render :new, alert: 'Failed to create sphere.'
+    # Add debugging to see what's happening
+    Rails.logger.debug "Service response: #{@sphere.inspect}"
+    Rails.logger.debug "Service errors: #{@sphere&.errors&.full_messages}"
+
+    respond_to do |format|
+      if @sphere&.persisted?  # Check if sphere was actually created and saved
+        format.turbo_stream { 
+          redirect_to sphere_path(@sphere), notice: 'Sphere was successfully created.' 
+        }
+        format.html { 
+          redirect_to sphere_path(@sphere), notice: 'Sphere was successfully created.' 
+        }
+      else
+        format.turbo_stream { 
+          render turbo_stream: turbo_stream.update('message', 
+            partial: 'shared/error_messages', 
+            locals: { message: 'Failed to create sphere.' })
+        }
+        format.html { 
+          redirect_to root_path, alert: 'Failed to create sphere.' 
+        }
+      end
     end
   end
 
